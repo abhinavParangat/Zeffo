b881af7fb579713d673dfcaa79f13d84a242032c
abhinav <abhinav.saini@parangat.com>
2023-01-11 12:03:17 +0530

    New Test Commit

--- Zeffo/Controller/SignUp/GiftCardVC.swift
+++ Zeffo/Controller/SignUp/GiftCardVC.swift
@@ -13,7 +13,7 @@ class GiftCardVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print("Hello")
+       
     }
  
     // MARK: - Button
